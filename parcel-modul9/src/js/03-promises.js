a68473baffb75ccb9f4ac3bc55f78d4cee590bce
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-02-22 18:58:45 +0200

    actualizae ex 3

--- parcel-modul9/src/js/03-promises.js
+++ parcel-modul9/src/js/03-promises.js
@@ -1,8 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-
-let form = document.querySelector(".form");
+//Resolvare exercitiu alt mod decat function createPromise(position, delay)
+/*let form = document.querySelector(".form");
 console.log(form)
 
 let firstDelay = document.querySelector('label [name="delay"]');
@@ -57,9 +57,42 @@ form.addEventListener("submit", (event)=>
                           
                         }
                       )
+*/
 
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
 
+const form = document.querySelector('.form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const firstDelay = parseInt(this.elements.delay.value);
+  const delayStep = parseInt(this.elements.step.value);
+  const amount = parseInt(this.elements.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const delay = firstDelay + (i - 1) * delayStep;
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
 
 
 
